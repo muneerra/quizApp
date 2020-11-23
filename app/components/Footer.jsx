6a414fa29fc6074ead1a60e8beb9cd302faea8d3
muneerra <munirahunaid09@gmail.com>
2020-11-23 23:39:01 +0500

    deploy my rca to ghp

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <p>Built with <span>&hearts;</span> by Munira Marvi </p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
